f7c7c87cbcca8442ffea7f1b0b2f29163316b5bd
Lewis Howles <lewishowles@users.noreply.github.com>
2024-08-14 18:06:14 +0100

    Fix incorrect command name

--- src/commands/get-form-field.js
+++ src/commands/get-form-field.js
@@ -2,6 +2,6 @@
  * Get the underlying form field (`input`, `select`, `textarea`) for a previous
  * subject.
  */
-Cypress.Commands.add("shouldNotBeVisible", { prevSubject: "element" }, (subject) => {
+Cypress.Commands.add("getFormField", { prevSubject: "element" }, (subject) => {
 	cy.wrap(subject).find("input, select, textarea");
 });
